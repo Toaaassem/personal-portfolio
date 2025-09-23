c7ee227412d54ab5705bc33d8e2b9471321d72e2
Toaaassem <toaa.assem33@gmail.com>
2025-09-24 02:28:02 +0300

    Updated: improved Getintouch form UI

--- src/component/BackgroundBeamsDemo.jsx
+++ src/component/BackgroundBeamsDemo.jsx
@@ -20,12 +20,24 @@ export function BackgroundBeamsDemo() {
           websites.
         </p>
         <div className=" flex flex-column justify-center">
-          <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50" onClick={() => scrollToSection("about")}>
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-10 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-              Discover More
-            </span>
-          </button>
+        <button
+  className="relative z-50 inline-flex h-10 w-50 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+  onClick={() =>
+    window.open(
+      "https://drive.google.com/file/d/1yK3VGN94w-cW-_UL8BEq3YmFofsia4pu/view?usp=sharing",
+      "_blank"
+    )
+  }
+>
+  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+  <span className="relative inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-10 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+    View CV
+  </span>
+</button>
+
+
+
+
         </div>
       </div>
       <BackgroundBeams />
